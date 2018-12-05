48a99f8b91e82c40b0838f8c8e61f115537ba595
janasja1 <janasja1@uhk.cz>
2018-12-05 14:44:50 +0100

    ukol 3 base

--- src/cz/uhk/fim/pixeltest/controller/Controller3D.java
+++ src/cz/uhk/fim/pixeltest/controller/Controller3D.java
@@ -4,27 +4,105 @@ import cz.uhk.fim.pixeltest.model3d.Cube;
 import cz.uhk.fim.pixeltest.model3d.Solid;
 import cz.uhk.fim.pixeltest.renderer.Renderer3D;
 import cz.uhk.fim.pixeltest.view.Raster;
+import transforms.Camera;
+import transforms.Mat4;
+import transforms.Mat4RotXYZ;
+import transforms.Vec3D;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Controller3D {
 
-    private Raster raster;
-    private Renderer3D renderer3D;
+    private int mx,my;
+    //private Raster raster;
+    private final Renderer3D renderer3D;
     private Solid cube;
+    private Camera camera;
 
     public Controller3D(Raster raster) {
-        this.raster = raster;
+        //this.raster = raster;
+        renderer3D = new Renderer3D(raster);
         initObjects();
-        initListeners();
-        renderer3D.draw(cube);
+        initListeners(raster);
     }
 
-    private void initListeners() {
-
+    private void resetCamera(){
+        camera = new Camera(
+                new Vec3D(0,-5,4),
+                Math.toRadians(90),//atimuth
+                Math.toRadians(-40),//zenith
+                1,true
+        );
+        renderer3D.setView(camera.getViewMatrix());
     }
 
     private void initObjects() {
-        renderer3D = new Renderer3D(raster);
+        //renderer3D = new Renderer3D(raster);
         cube = new Cube();
+        renderer3D.add(cube);
+        resetCamera();
+    }
+
+    private void initListeners(Raster raster) {
+        raster.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mx = e.getX();
+                my = e.getY();
+            }
+        });
+
+        raster.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)){
+                    double diff = (mx - e.getX()) / 200.0;
+                    double azimuth = camera.getAzimuth() + diff;
+                    camera = camera.withAzimuth(azimuth);
+                    //dodělat zenit
+                    double diff2 = my - e.getY() / 200.0;
+                    double zenit = camera.getZenith() + diff2;
+                    /*
+                    if (zenit>=0){
+                        zenit = Math.min(((my - e.getY()) / 200.0),Math.PI/2);
+                    }else {
+                        zenit = Math.max(((my - e.getY()) / 200.0),-(Math.PI/2));
+                    }*/
+                    System.out.println(zenit);
+                    System.out.println(azimuth);
+                    camera = camera.withZenith(zenit);
+                    renderer3D.setView(camera.getViewMatrix());
+                }
+
+                else if (SwingUtilities.isRightMouseButton(e)){
+                    double rotX = (mx - e.getX())/-200.0;
+                    double rotY = (my - e.getY())/-200.0;
+                    Mat4 rot = renderer3D.getModel().mul(new Mat4RotXYZ(rotY,0,rotX));
+                    renderer3D.setModel(rot);
+                }
+
+                mx = e.getX();
+                my = e.getY();
+            }
+        });
+
+        raster.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_UP:
+                        case KeyEvent.VK_W:
+                            camera = camera.forward(1);
+                            renderer3D.setView(camera.getViewMatrix());
+                            break;
+                            //doplnit ovladani
+                }
+            }
+        });
     }
 
 }
--- src/cz/uhk/fim/pixeltest/renderer/Renderer3D.java
+++ src/cz/uhk/fim/pixeltest/renderer/Renderer3D.java
@@ -5,6 +5,8 @@ import cz.uhk.fim.pixeltest.view.Raster;
 import transforms.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Renderer3D {
@@ -13,6 +15,7 @@ public class Renderer3D {
     private Mat4 model;
     private Mat4 view;
     private Mat4 projection;
+    private List<Solid> solids = new ArrayList<>();
 
     public Renderer3D(Raster raster) {
         this.raster = raster;
@@ -32,7 +35,13 @@ public class Renderer3D {
                 200);
     }
 
-    public void draw(Solid... solids) {
+    public void add(Solid...solids){
+        this.solids.addAll(Arrays.asList(solids));
+        repaint();
+    }
+
+    public void repaint() {
+        raster.clear();
         for (Solid solid : solids
         ) {
             List<Point3D> vertices = solid.getVertices();
@@ -51,32 +60,52 @@ public class Renderer3D {
         a = a.mul(model).mul(view).mul(projection);
         b = b.mul(model).mul(view).mul(projection);
 
-        Vec3D v1, v2;
-        if (a.dehomog().isPresent()) {
-            v1 = a.dehomog().get();
-        } else {
-            v1 = new Vec3D(0);
-        }
-        if (b.dehomog().isPresent()) {
-            v2 = b.dehomog().get();
-        } else {
-            v2 = new Vec3D(0);
-        }
 
-        if (Math.abs(v1.getX()) > 1 || Math.abs(v1.getY()) > 1 || v1.getZ() < 0 || v1.getZ() > 1) {
-            return;
-        }
-        if (Math.abs(v2.getX()) > 1 || Math.abs(v2.getY()) > 1 || v2.getZ() < 0 || v2.getZ() > 1) {
+        if (!a.dehomog().isPresent()||!b.dehomog().isPresent()){
             return;
         }
 
-        v1 = v1.add(new Vec3D(1, 1, 0))
-                .mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
-        v2 = v2.add(new Vec3D(1, 1, 0))
-                .mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
+        Vec3D v1 = a.dehomog().get();
+        Vec3D v2 = b.dehomog().get();
+
+        if (Math.abs(v1.getX()) > 1 || Math.abs(v1.getY()) > 1 || v1.getZ() < 0 || v1.getZ() > 1) return;
+        if (Math.abs(v2.getX()) > 1 || Math.abs(v2.getY()) > 1 || v2.getZ() < 0 || v2.getZ() > 1) return;
+
+        v1 = transformToWindow(v1);
+        v2 = transformToWindow(v2);
 
         raster.drawLine(v1.getX(), v1.getY(), v2.getX(), v2.getY(), color);
     }
 
+    private Vec3D transformToWindow(Vec3D v){
+        return v.mul(new Vec3D(1,-1,1))
+                .add(new Vec3D(1, 1, 0))
+                .mul(new Vec3D(Raster.WIDTH / 2f, Raster.HEIGHT / 2f, 1));
+    }
 
+    public Mat4 getModel() {
+        return model;
+    }
+
+    public void setModel(Mat4 model) {
+        this.model = model;
+        repaint();
+    }
+
+    public Mat4 getView() {
+        return view;
+    }
+
+    public void setView(Mat4 view) {
+        this.view = view;
+        repaint();
+    }
+
+    public Mat4 getProjection() {
+        return projection;
+    }
+
+    public void setProjection(Mat4 projection) {
+        this.projection = projection;
+    }
 }
